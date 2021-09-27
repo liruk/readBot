88348b835ed526295bd12533524bcf28cb349890
liruk <34234523+liruk@users.noreply.github.com>
2021-09-27 14:20:06 +0900

    voicevox用に変更

--- read_bot.py
+++ read_bot.py
@@ -1,9 +1,5 @@
 import discord
 from discord.ext import commands
-import asyncio
-import os
-import subprocess
-import ffmpeg
 from voice_generator import creat_WAV
 
 client = commands.Bot(command_prefix='.')
@@ -26,30 +22,25 @@ async def join(ctx):
     print('#voicechannelに接続')
     await vc.connect()
 
+
 @client.command()
 async def bye(ctx):
     print('#bye')
     print('#切断')
     await ctx.voice_client.disconnect()
 
-@client.command()
-async def register(ctx, arg1, arg2):
-    with open('C:/open_jtalk/bin/dic.txt', mode='a') as f:
-        f.write('\n'+ arg1 + ',' + arg2)
-        print('dic.txtに書き込み：''\n'+ arg1 + ',' + arg2)
-    await ctx.send('`' + arg1+'` を `'+arg2+'` として登録しました')
 
 @client.event
 async def on_voice_state_update(member, before, after):
     server_id_test = "サーバーID"
     text_id_test = "通知させたいテキストチャンネルID"
 
-
     if member.guild.id == server_id_test:   # サーバーid
         text_ch = client.get_channel(text_id_test)   # 通知させたいTEXTチャンネルid
         if before.channel is None:
             msg = f'【VC参加ログ】{member.name} が {after.channel.name} に参加しました。'
             await text_ch.send(msg)
+
 
 @client.event
 async def on_message(message):
@@ -61,14 +52,15 @@ async def on_message(message):
 
     else:
         if message.guild.voice_client:
-            print('#message.content:'+ message.content)
+            print('#message.content:' + message.content)
             creat_WAV(message.content)
             source = discord.FFmpegPCMAudio("output.wav")
             message.guild.voice_client.play(source)
+            # wav_fp.close()
         else:
             pass
     await client.process_commands(message)
     print('---on_message_end---')
 
 
-client.run("トークン")
+client.run("ボットのトークンを入れる")
--- voice_generator.py
+++ voice_generator.py
@@ -1,71 +1,60 @@
-import subprocess
 import re
+import requests
+
 
 # ************************************************
 # remove_custom_emoji
 # 絵文字IDは読み上げない
 # ************************************************
+
+
 def remove_custom_emoji(text):
-    
-    #pattern = r'<:[a-zA-Z0-9_]+:[0-9]+>'    # カスタム絵文字のパターン
+
+    # pattern = r'<:[a-zA-Z0-9_]+:[0-9]+>'    # カスタム絵文字のパターン
     pattern = r'<:'    # カスタム絵文字のパターン
-    text = re.sub(pattern,'',text)   # 置換処理
+    text = re.sub(pattern, '', text)   # 置換処理
     pattern = r':[0-9]+>'    # カスタム絵文字のパターン
-    return re.sub(pattern,'',text)   # 置換処理
+    return re.sub(pattern, '', text)   # 置換処理
 
 # ************************************************
 # url_shouryaku
 # URLなら省略
 # ************************************************
+
+
 def url_shouryaku(text):
     pattern = "https?://[\w/:%#\$&\?\(\)~\.=\+\-]+"
-    return re.sub(pattern,'URLは省略するのデス！',text)   # 置換処理
+    return re.sub(pattern, 'URLは省略するのデス！', text)   # 置換処理
 
 # ************************************************
 # remove_picture
 # 画像ファイルなら読み上げない
 # ************************************************
+
+
 def remove_picture(text):
     pattern = r'.*(\.jpg|\.jpeg|\.gif|\.png|\.bmp)'
-    return re.sub(pattern,'',text)   # 置換処理
+    return re.sub(pattern, '', text)   # 置換処理
 
 # ************************************************
 # remove_command
 # コマンドは読み上げない
 # ************************************************
+
+
 def remove_command(text):
     pattern = r'^\!.*'
-    return re.sub(pattern,'',text)   # 置換処理
+    return re.sub(pattern, '', text)   # 置換処理
 
 # ************************************************
 # remove_log
 # 参加ログは読み上げない
 # ************************************************
+
+
 def remove_log(text):
     pattern = r'(\【VC参加ログ\】.*)'
-    return re.sub(pattern,'',text)   # 置換処理
-
-# ************************************************
-# user_custam
-# ユーザ登録した文字を読み替える
-# ************************************************
-def user_custam(text):
-
-    f = open('C:/open_jtalk/bin/dic.txt', 'r')
-    line = f.readline()
-
-    while line:
-        pattern = line.strip().split(',')
-        if pattern[0] in text:
-            text = text.replace(pattern[0], pattern[1])
-            print('置換後のtext:'+text)
-            break
-        else:
-            line = f.readline()
-    f.close()
-
-    return text
-
+    return re.sub(pattern, '', text)   # 置換処理
 
 
 # ************************************************
@@ -74,47 +63,38 @@ def user_custam(text):
 # 引数：inputText
 # 書き込みファイル：input.txt、output.wav
 # ************************************************
+
+
 def creat_WAV(inputText):
-        # message.contentをテキストファイルに書き込み
 
     inputText = remove_custom_emoji(inputText)   # 絵文字IDは読み上げない
     inputText = remove_command(inputText)   # コマンドは読み上げない
     inputText = url_shouryaku(inputText)   # URLなら省略
     inputText = remove_picture(inputText)   # 画像なら読み上げない
     inputText = remove_log(inputText)   # 参加ログなら読み上げない
-    inputText = user_custam(inputText)   # ユーザ登録した文字を読み替える
-    input_file = 'input.txt'
 
-    with open(input_file,'w',encoding='shift_jis') as file:
-        file.write(inputText)
+    payload = {'text': inputText, 'speaker': 0}
+    headers = {'accept': 'application/json'}
+    r = requests.post('http://127.0.0.1:50021/audio_query',
+                      params=payload, headers=headers)
+    queryDict = r.json()
 
-    command = 'C:/open_jtalk/bin/open_jtalk -x {x} -m {m} -r {r} -ow {ow} {input_file}'
+    queryDict['kana'] = ""  # ここを虚無にしないとエスケープ付近が解読できずに422エラーが起きて処理が終わる
 
-    #辞書のPath
-    x = 'C:/open_jtalk/bin/dic'
+    payload = {'speaker': 0}
+    headers = {'accept': 'audio/wav', 'Content-Type': 'application/json'}
+    audio = requests.post('http://127.0.0.1:50021/synthesis',
+                          params=payload, headers=headers, json=queryDict)
+    audio.raise_for_status()
 
-    #ボイスファイルのPath
-    #m = 'C:/open_jtalk/bin/nitech_jp_atr503_m001.htsvoice'
-    #m = 'C:/open_jtalk/bin/mei/mei_sad.htsvoice'
-    #m = 'C:/open_jtalk/bin/mei/mei_angry.htsvoice'
-    m = 'C:/open_jtalk/bin/mei/mei_bashful.htsvoice'
-    #m = 'C:/open_jtalk/bin/mei/mei_happy.htsvoice'
-    #m = 'C:/open_jtalk/bin/mei/mei_normal.htsvoice'
-
-    #発声のスピード
-    #r = '2.0'
-    r = '1.2'
-
-    #出力ファイル名　and　Path
+    # 出力ファイル名　and　Path
     ow = 'output.wav'
+    with open(ow, 'wb') as file:
+        file.write(audio.content)
 
-    args= {'x':x, 'm':m, 'r':r, 'ow':ow, 'input_file':input_file}
-
-    cmd= command.format(**args)
-    print(cmd)
-
-    subprocess.run(cmd)
+    # return audio.content
     return True
+
 
 if __name__ == '__main__':
     creat_WAV('テスト')
